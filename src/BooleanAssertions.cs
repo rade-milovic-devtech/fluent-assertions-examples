5676a6b38c3744e14696e7cd1e559329e4486fc2
Rade Milovic <rade.milovic@devtechgroup.com>
2017-02-27 23:06:25 +0100

    Adds boolean assertions examples.

---
+++ src/BooleanAssertions.cs
@@ -0,0 +1,31 @@
+﻿using FluentAssertions;
+using Xunit;
+
+namespace FluentAssertionsExamples
+{
+    public class BooleanAssertions
+    {
+        [Fact]
+        public void Boolean_assertions()
+        {
+            bool theBoolean = false;
+
+            theBoolean.Should().BeFalse();
+
+            theBoolean = true;
+            bool otherBoolean = true;
+
+            theBoolean.Should().BeTrue();
+            theBoolean.Should().Be(otherBoolean);
+        }
+
+        [Fact]
+        public void Nullable_boolean_assertions()
+        {
+            bool? theBoolean = null;
+
+            theBoolean.Should().NotBeFalse();
+            theBoolean.Should().NotBeTrue();
+        }
+    }
+}
