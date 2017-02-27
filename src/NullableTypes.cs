686c1b97d67efbd896026d0fe0576f8bf7e08cc8
Rade Milovic <rade.milovic@devtechgroup.com>
2017-02-27 23:00:38 +0100

    Adds nullable types assertions examples.

---
+++ src/NullableTypes.cs
@@ -0,0 +1,20 @@
+﻿using FluentAssertions;
+using Xunit;
+
+namespace FluentAssertionsExamples
+{
+    public class NullableTypes
+    {
+        [Fact]
+        public void Nullable_types_assertions()
+        {
+            short? theShort = null;
+            theShort.Should().NotHaveValue();
+            theShort.Should().BeNull();
+
+            int? theInt = 3;
+            theInt.Should().HaveValue();
+            theInt.Should().NotBeNull();
+        }
+    }
+}
